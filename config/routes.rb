b4c0b0b4d0936b059f9e642fd713e6c3a8983294
Silviu Iordan <silviu.iordan@yahoo.com>
2021-08-15 16:27:57 +0300

    clean up nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :user_stocks
   devise_for :users
   root 'home#index'
-  get 'my_porfolio', to: 'users#my_portfolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
