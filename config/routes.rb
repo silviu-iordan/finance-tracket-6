8cfcae1f731d5a18cd45e5abd23da2661d98b02f
silviu-iordan <silviu.iordan@yahoo.com>
2021-08-23 16:31:58 +0300

    add  profile of a friend, delete follower of a friend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'home#index'
@@ -7,5 +8,7 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 
 end
